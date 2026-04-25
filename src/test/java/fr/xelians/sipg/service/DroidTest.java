51137d72b486d0a2d23232525135d56d21331bc8
Emmanuel Deviller <emmanuel.deviller@xelians.fr>
2026-04-25 19:48:37 +0200

    chore: improve test logging and structure using JUnit 5 extensions

--- src/test/java/fr/xelians/sipg/service/DroidTest.java
+++ src/test/java/fr/xelians/sipg/service/DroidTest.java
@@ -27,8 +27,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResult;
 
 /**
@@ -39,42 +37,32 @@ import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResult;
 @ExtendWith(TestInit.class)
 class DroidTest {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DroidTest.class);
-
   /** Test small pdf droid. */
   @Test
   void testSmallPdfDroid() {
     Path path = Paths.get(TestInit.TEST_RESOURCES + "dummy.pdf");
     List<IdentificationResult> results = DroidUtils.matchBinarySignatures(path, "pdf");
     assertEquals(1, results.size());
-    assertEquals("fmt/18", results.get(0).getPuid());
+    assertEquals("fmt/18", results.getFirst().getPuid());
   }
 
-  /**
-   * Test medium pdf droid.
-   *
-   * @throws Exception the exception
-   */
+  /** Test medium pdf droid. */
   @Test
   void testMediumPdfDroid() {
     Path path = Paths.get(TestInit.TEST_RESOURCES + "citizenfour.pdf");
     List<IdentificationResult> results = DroidUtils.matchBinarySignatures(path, "pdf");
     assertEquals(1, results.size());
-    assertEquals("fmt/19", results.get(0).getPuid());
+    assertEquals("fmt/19", results.getFirst().getPuid());
   }
 
-  /**
-   * Test loop droid.
-   *
-   * @throws Exception the exception
-   */
+  /** Test loop droid. */
   @Test
   void testLoopDroid() {
     Path path = Paths.get(TestInit.TEST_RESOURCES + "citizenfour.pdf");
 
     for (int i = 0; i < 1000; i++) {
       List<IdentificationResult> results = DroidUtils.matchBinarySignatures(path, "pdf");
-      assertEquals("fmt/19", results.get(0).getPuid());
+      assertEquals("fmt/19", results.getFirst().getPuid());
     }
   }
 }
